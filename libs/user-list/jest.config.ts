edd3065eb8427753a72ce58dd4c3cd6f45c9d517
Nathan Skelley <nathan@heytempo.com>
2022-12-15 13:39:52 +0000

    Create new demo lib and add storybook

---
+++ libs/user-list/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'user-list',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/user-list',
+};
