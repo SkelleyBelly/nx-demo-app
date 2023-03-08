12dd2ea5d144bf214adc568a895d92a9f185d1ed
Nathan Skelley <nathan@heytempo.com>
2023-03-08 11:19:01 +0000

    Added denormalised sections to the schema

--- apps/mongo-graphql/prisma/seed.ts
+++ apps/mongo-graphql/prisma/seed.ts
@@ -1,16 +1,46 @@
 import { PrismaClient, Status } from '../src/generated/prisma';
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 import { ObjectId } from 'bson'
 
 const prisma = new PrismaClient()
 
 async function main() {
 
-  const users = new Array(500_000).fill(undefined).map((_, index) => ({
+  const users = new Array(250_000).fill(undefined).map((_, index) => ({
     id: new ObjectId().toHexString(),
     name: faker.name.fullName(),
     email: faker.internet.email()
   }));
+
+  const jobs = new Array(10_000).fill(undefined).map((_, index) => ({
+    id: new ObjectId().toHexString(),
+    title: faker.name.jobTitle()
+  }));
+
+  const applications = new Array(50_000).fill(undefined).map((_, index) => {
+
+    const applicationId = new ObjectId().toHexString()
+
+    const candidate = users[index]
+    const job = jobs[Math.floor(index / 5)];
+
+    (candidate as any).applications = [{
+      id: applicationId,
+      jobId: job.id,
+      status: Status.applied,
+    }]
+    
+    return {
+      id: applicationId,
+      jobId: job.id,
+      status: Status.applied,
+      candidateId: candidate.id,
+      candidate: {
+        id: candidate.id,
+        name: candidate.name
+      }
+    }
+  })
 
   await prisma.user.createMany({
     data: users,
@@ -18,22 +48,11 @@ async function main() {
 
   console.log("USERS ADDED")
 
-  const jobs = new Array(50).fill(undefined).map((_, index) => ({
-    id: new ObjectId().toHexString(),
-    title: faker.name.jobTitle()
-  }));
-
   await prisma.job.createMany({
     data: jobs
   })
 
   console.log("JOBS ADDED")
-
-  const applications = new Array(50_000).fill(undefined).map((_, index) => ({
-    candidateId: users[index].id,
-    jobId: jobs[Math.floor(index / 1000)].id,
-    status: Status.applied
-  }))
 
   await prisma.application.createMany({
     data: applications,
--- apps/mongo-graphql/src/main.ts
+++ apps/mongo-graphql/src/main.ts
@@ -1,13 +1,53 @@
-const { applyMiddleware } = require("graphql-middleware");
 import 'reflect-metadata'
 import * as tq from 'type-graphql'
 import { ApolloServer } from 'apollo-server'
 import { PrismaClient } from './generated/prisma';
-import { resolvers } from "./generated/typegraphql";
-import { getUser } from './getUser';
-import { GraphQLError } from 'graphql';
-import { customAuthChecker } from './authChecker';
-import { permissions } from './permissions';
+import { applyResolversEnhanceMap, resolvers, ResolversEnhanceMap, UpdateOneApplicationArgs, UpdateOneUserArgs } from "./generated/typegraphql";
+import { MiddlewareFn, UseMiddleware } from 'type-graphql';
+import { inspect } from "util";
+
+
+const updateUser: MiddlewareFn = async ({ args }, next) => {
+
+  const updatedName = (args as UpdateOneUserArgs).data.name?.set;
+
+  const res = await next()
+
+  if (updatedName) {
+    const user = await prisma.user.findFirst({ where: args.where, select: { id: true, applications: true } })
+    await prisma.application.updateMany({ where: { id: { in: user.applications.map(({ id }) => id) } }, data: { candidate: { set: { id: user.id, name: updatedName } } } })
+  }
+
+  return res;
+}
+
+const updateApplication: MiddlewareFn = async ({ args }, next) => {
+
+  const updatedStatus = (args as UpdateOneApplicationArgs).data.status?.set
+
+  const res = await next()
+
+  if (updatedStatus) {
+    const application = await prisma.application.findFirst({ where: args.where, select: { id: true, candidate: true } })
+
+    const candidate = await prisma.user.findFirst({ where: { id: application.candidate.id }, select: { id: true, applications: true } })
+
+    await prisma.user.update({ where: { id: candidate.id }, data: { applications: candidate.applications.map(app => ({ ...app, status: app.id === application.id ? updatedStatus : app.status })) } })
+  }
+
+  return res;
+}
+
+const resolversEnhanceMap: ResolversEnhanceMap = {
+  User: {
+    updateOneUser: [UseMiddleware(updateUser)],
+  },
+  Application: {
+    updateOneApplication: [UseMiddleware(updateApplication)]
+  }
+}
+
+applyResolversEnhanceMap(resolversEnhanceMap)
 
 const PORT = '3333'
 
